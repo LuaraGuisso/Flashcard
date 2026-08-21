6ff33f5b33748e04d22faa715fe2bd8d3b58299b
LuaraGuisso <luara.guisso.lima@escola.pr.gov.br>
2026-08-21 15:55:56 -0300

    .

--- scritp.js
+++ scritp.js
@@ -1,7 +1,12 @@
 const cards = document.querySelectorAll(".card");
 
 cards.forEach(function(card) {
+
     card.addEventListener("click", function() {
+
         card.classList.toggle("clicado");
+
     });
+
 });
+
